3e38c29d5b9ff1a0fdbf152f1a9183e734c5f661
Avijit Dash <Avijit009>
2023-09-19 20:00:58 +0600

    Making the Cart Quantity interactive.

--- script/amazon.js
+++ script/amazon.js
@@ -81,9 +81,19 @@ document.querySelectorAll('.add-to-cart')
                 productId: productId,
                 quantity: 1
             });
-            console.log(cart);
-        }
+        
+            let cartQuantity = 0;
 
+            cart.forEach((item) => {
+                cartQuantity += item.quantity;
+            });
+
+            document.querySelector(".added-cart-quantity")
+            .innerHTML = cartQuantity;
+
+            // console.log(cartQuantity);
+        }
+        // console.log(cart);
     });
 
 });
