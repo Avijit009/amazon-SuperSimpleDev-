d9c567ca291461122eaeeb739afbb6360934b99a
Avijit Dash <Avijit009>
2023-09-20 20:40:50 +0600

    save cart in local storage.

--- data/cart.js
+++ data/cart.js
@@ -1,25 +1,24 @@
-export let cart = [
-    {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity: 2,
-    },
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+}
 
-    {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1,
-    },
-
-    {
-        productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-        quantity: 3,
-    }
-];
+function saveToStorage(){
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId){
     let matchingItem;
 
     cart.forEach((cartItem) => {
-        if (productId === item.productId){
+        if (productId === cartItem.productId){
             matchingItem = cartItem;
         }
     });
@@ -37,10 +36,13 @@ export function addToCart(productId){
             quantity: quantity
         });
     }
+
+    saveToStorage();
 }
 
 export function deleteCartItem(productId){
     const newCart = [];
+
     cart.forEach((cartItem) => {
         if (cartItem.productId !== productId){
             newCart.push(cartItem);
@@ -48,4 +50,6 @@ export function deleteCartItem(productId){
     });
 
     cart = newCart;
+
+    saveToStorage();
 }
